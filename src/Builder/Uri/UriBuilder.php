fdf335fd7322867fb9737fd7f939899346df5cfd
Rick in t Veld <rick.in.t.veld@mywheels.nl>
2024-08-06 15:16:29 +0200

    [TST-1]remove docblock parameter

--- src/Builder/Uri/UriBuilder.php
+++ src/Builder/Uri/UriBuilder.php
@@ -9,7 +9,6 @@ use App\Exception\ClassNotFoundException;
 class UriBuilder
 {
     /**
-     * @param string $builder
      * @param array<mixed> $parameters
      */
     public function build(string $builder, array $parameters): string
